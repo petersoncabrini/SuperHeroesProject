6776a6eeb8079db06be11c9f02a317a0ee40b871
Peterson Cabrini <petersoncabrini@hotmail.com>
2022-11-02 18:00:19 -0300

    add SuperHero class to API project

---
+++ SuperHeroAPI/Models/SuperHero.cs
@@ -0,0 +1,12 @@
+namespace SuperHeroAPI.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FistName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+
+    }
+}
