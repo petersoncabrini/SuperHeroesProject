d960cc2edabf63dc39c741667c33b0c20d775f77
Peterson Cabrini <petersoncabrini@hotmail.com>
2022-11-05 11:07:57 -0300

    changed PostgreSQL to SQL Server connections configuration

--- SuperHeroAPI/Program.cs
+++ SuperHeroAPI/Program.cs
@@ -1,11 +1,17 @@
+using SuperHeroAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 builder.Services.AddCors(options => options.AddPolicy(name: "Superhero",
     policy =>
     {
