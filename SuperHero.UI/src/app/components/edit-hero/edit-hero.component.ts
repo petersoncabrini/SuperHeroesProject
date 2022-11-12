caed26577de540c3a0a56c7e400f6d0999582122
Peterson Cabrini <petersoncabrini@hotmail.com>
2022-11-11 22:02:57 -0300

    call the service in edit-hero component

--- SuperHero.UI/src/app/components/edit-hero/edit-hero.component.ts
+++ SuperHero.UI/src/app/components/edit-hero/edit-hero.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { SuperHero } from 'src/app/models/super-hero';
+import { SuperHeroService } from 'src/app/services/super-hero.service';
 
 @Component({
   selector: 'app-edit-hero',
@@ -9,22 +10,29 @@ import { SuperHero } from 'src/app/models/super-hero';
 export class EditHeroComponent implements OnInit {
 
   @Input() hero?: SuperHero;
+  @Output() heroesUpdated = new EventEmitter<SuperHero[]>();
 
-  constructor() { }
+  constructor(private superHeroService: SuperHeroService) { }
 
   ngOnInit(): void {
   }
 
   updateHero(hero: SuperHero) {
-
+    this.superHeroService
+      .updateSuperHero(hero)
+      .subscribe((heroes: SuperHero[]) => this.heroesUpdated.emit(heroes))
   }
 
   deleteHero(hero: SuperHero) {
-
+    this.superHeroService
+      .deleteSuperHero(hero)
+      .subscribe((heroes: SuperHero[]) => this.heroesUpdated.emit(heroes))
   }
 
   createHero(hero: SuperHero) {
-
+    this.superHeroService
+      .createSuperHero(hero)
+      .subscribe((heroes: SuperHero[]) => this.heroesUpdated.emit(heroes))
   }
 
 }
